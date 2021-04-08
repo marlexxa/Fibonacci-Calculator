4d9a2420fff39ba4ba1814dacf3e2f3e2b49f791
marlexxa <marlexxa@gmail.com>
2021-04-08 23:23:55 +0200

    refactor: move App styles to separate file

--- src/views/App.js
+++ src/views/App.js
@@ -1,17 +1,9 @@
 import React from 'react';
-import styled, { ThemeProvider } from 'styled-components';
+import { ThemeProvider } from 'styled-components';
 import Calculator from 'components/organisms/Calculator/Calculator';
 import { GlobalStyle } from 'assets/styles/GlobalStyle';
 import { theme } from 'assets/styles/theme';
-
-const Wrapper = styled.div`
-  background-color: ${({ theme }) => theme.colors.lightGrey};
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 100%;
-  height: 100vh;
-`;
+import { Wrapper } from './App.styles';
 
 const App = () => (
   <ThemeProvider theme={theme}>
